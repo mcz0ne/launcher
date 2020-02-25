6778bdff45b2ac27e5c23031759e7b99f62d4c9e
Christopher König <iam@z0ne.moe>
2020-02-25 17:59:24 +0100

    Add (gui): Almost finished main window layout

--- src/main/kotlin/view/MainView.kt
+++ src/main/kotlin/view/MainView.kt
@@ -4,14 +4,18 @@ import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
+import javafx.geometry.VPos
+import javafx.scene.control.SelectionMode
 import javafx.scene.control.TabPane
 import javafx.scene.layout.GridPane
 import javafx.scene.layout.Priority
+import javafx.scene.layout.RowConstraints
+import javafx.scene.text.FontWeight
 import mu.KotlinLogging
 import tornadofx.*
 
-class MainView:View("mc.z0ne.moe Launcher") {
-    private val logger = KotlinLogging.logger{}
+class MainView : View("mc.z0ne.moe Launcher") {
+    private val logger = KotlinLogging.logger {}
 
     private val optModel = object : ViewModel() {
         val minMem = bind { SimpleStringProperty() }
@@ -26,36 +30,81 @@ class MainView:View("mc.z0ne.moe Launcher") {
         val height = bind { SimpleIntegerProperty() }
     }
 
-    override val root = borderpane {
+    override val root = vbox {
         setPrefSize(800.0, 600.0)
         minWidth = 600.0
         minHeight = 400.0
 
-        top = tabpane {
-            useMaxWidth = true
-            useMaxHeight = true
+        tabpane {
+            vgrow = Priority.ALWAYS
             tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
 
             tab("News") {
                 webview {
-                    useMaxWidth = true
-                    useMaxHeight = true
-
                     engine.load("https://mc.z0ne.moe")
                 }
             }
+
             tab("Mods")
-            tab("Progress")
-            tab("Logs")
+
+            tab("Logs") {
+                vbox {
+                    paddingAll = 5.0
+                    spacing = 10.0
+
+                    textarea {
+                        vgrow = Priority.ALWAYS
+                    }
+
+                    buttonbar {
+                        button("Copy")
+                        button("Save")
+                        button("Upload")
+                    }
+                }
+            }
 
             tab("Accounts") {
+                vbox {
+                    paddingAll = 5.0
+                    spacing = 10.0
 
+                    listview<String> {
+                        vgrow = Priority.ALWAYS
+                        cellFormat {
+                            text = it
+
+                            style {
+                                if (it == "Gamma") {
+                                    fontWeight = FontWeight.BOLD
+                                }
+                            }
+                        }
+
+                        items.add("Alpha")
+                        items.add("Beta")
+                        items.add("Gamma")
+                        selectionModel.selectionMode = SelectionMode.SINGLE
+                    }
+
+                    buttonbar {
+                        vgrow = Priority.NEVER
+                        button("Add new Account")
+                    }
+                }
             }
 
             tab("Options") {
                 form {
+                    paddingAll = 5.0
+
                     fieldset {
-                        legend = label("Minecraft")
+                        legend = label("Minecraft") {
+                            style {
+                                fontScale = 1.1
+                                fontWeight = FontWeight.BOLD
+                            }
+                        }
 
                         field("Window Width") {
                             textfield(mcModel.width)
@@ -71,7 +120,12 @@ class MainView:View("mc.z0ne.moe Launcher") {
                     }
 
                     fieldset {
-                        legend = label("Java")
+                        legend = label("Java") {
+                            style {
+                                fontScale = 1.1
+                                fontWeight = FontWeight.BOLD
+                            }
+                        }
 
                         field("Minimum Memory") {
                             textfield(optModel.minMem)
@@ -91,23 +145,75 @@ class MainView:View("mc.z0ne.moe Launcher") {
                         }
                     }
 
-                    hbox {
+                    buttonbar {
                         button("Save") {
                             isDefaultButton = true
                         }
                         button("Cancel") {
                             isCancelButton = true
                         }
-                        button("Reset to Defaults") {}
+                        button("Reset to Defaults")
+                    }
+                }
+            }
+
+            tab("About") {
+                vbox {
+                    paddingAll = 5.0
+                    spacing = 10.0
+
+                    label("mcz Launcher") {
+                        style {
+                            fontScale = 1.5
+                            fontWeight = FontWeight.BOLD
+                        }
+                    }
+
+                    gridpane {
+                        constraintsForColumn(0).prefWidth = 110.0
+                        val rc = RowConstraints(30.0)
+                        rc.valignment = VPos.TOP
+                        rowConstraints.setAll(rc, rc, rc, rc)
+
+                        row {
+                            label("Version")
+                            label("x.y.z-suffix (hash)")
+                        }
+
+                        row {
+                            label("License")
+                            label("Apache 2.0")
+                        }
+
+                        row {
+                            label("Source Code")
+                            hyperlink("https://github.com/mcz0ne/launcher") {
+                                paddingAll = 0.0
+                            }
+                        }
+
+                        row {
+                            label("Authors")
+                            vbox {
+                                textflow {
+                                    label("Kura Bloodlust (")
+                                    hyperlink("https://github.com/cking") {
+                                        paddingAll = 0.0
+                                    }
+                                    label(")")
+                                }
+
+                            }
+                        }
                     }
                 }
             }
         }
 
-        bottom = hbox {
-            useMaxWidth = true
-            minHeight = 200.0
-            maxHeight = 200.0
+        hbox {
+            vgrow = Priority.NEVER
+            minHeight = 50.0
+            maxHeight = 50.0
 
             label("l")
             label("m")
